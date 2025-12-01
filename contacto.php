726ddacfaf282c6bce14b9e6f1eed5a13b8a26d7
[Rafael Montes] <rafaelmontesjimenez@gmail.com>
2025-12-01 14:37:03 +0100

    se puede enviar correo desde contacto pinchando el enlace

--- contacto.php
+++ contacto.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="es">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
@@ -8,17 +9,18 @@
     <link rel="stylesheet" href="css/footer.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="css/contacto.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="css/comun.css?v=<?php echo time(); ?>">
-    <link rel="shortcut icon" href="img/Logo-reformas-asier.png"/>
+    <link rel="shortcut icon" href="img/Logo-reformas-asier.png" />
 </head>
+
 <body>
-   <header>
+    <header>
         <div class="container">
-           <?php include 'includes/nav.php'; ?>
+            <?php include 'includes/nav.php'; ?>
         </div>
     </header>
 
     <main class="container" role="main">
-<section id="contacto" class="contact">
+        <section id="contacto" class="contact">
             <h1>Contacto y presupuesto</h1>
             <p class="muted">Contacta por teléfono o por email para una visita sin compromiso.</p>
 
@@ -26,12 +28,18 @@
                 <h2>Teléfono</h2>
                 <p><a href="tel:+34634261921">+34 634 26 19 21</a></p>
 
-                <h2>Email</h2>
+                <h2>Pincha en uno de estos Email para ponerte en contacto</h2>
                 <p><a href="mailto:Isidro_asijes@hotmail.com">Isidro_asijes@hotmail.com</a></p>
+                <p>
+                    <a href="https://mail.google.com/mail/?view=cm&fs=1&to=isidrorasier@gmail.com">
+                        isidrorasier@gmail.com
+                    </a>
+                </p>
             </div>
-        </section>        
+        </section>
     </main>
 
     <?php include 'includes/footer.php'; ?>
 </body>
+
 </html>
